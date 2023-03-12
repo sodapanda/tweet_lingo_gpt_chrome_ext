431cc39e6992dc6d63583f1d953ee30ffebb7fca
leucas <sodapanda20@gmail.com>
2023-03-12 11:33:34 +0800

    A

--- contents/multiple.tsx
+++ contents/multiple.tsx
@@ -1,4 +1,3 @@
-import { Box, Button, Paper, Text, createEmotionCache } from "@mantine/core"
 import { format } from "date-fns"
 import type {
   PlasmoCSConfig,
@@ -11,11 +10,6 @@ import { useState } from "react"
 import { Storage } from "@plasmohq/storage"
 
 import { ThemeProvider } from "~theme"
-
-export const getStyle = () => document.createElement("style")
-export const config: PlasmoCSConfig = {
-  matches: ["https://twitter.com/*"]
-}
 
 export const getInlineAnchorList: PlasmoGetInlineAnchorList = () => {
   const shareButtonList = document.querySelectorAll('[data-testid="tweetText"]')
@@ -57,93 +51,83 @@ function getLeafNodeTextContent(rootElement: any): string {
 }
 
 const PlasmoInline: FC<PlasmoCSUIProps> = ({ anchor }) => {
-  const styleCache = createEmotionCache({
-    key: "plasmo-mui-cache",
-    prepend: true,
-    container:
-      anchor.element.nextElementSibling.shadowRoot.querySelector("style")
-  })
-
   const tweetHolder = anchor.element
   const [extMsg, setExt] = useState("")
   const [isLoading, setIsLoading] = useState(false)
 
   return (
-    <ThemeProvider emotionCache={styleCache}>
-      <Box component="div">
-        {extMsg ? (
-          <Paper shadow="sm" radius="md" p="md" withBorder>
-            <Text>{extMsg}</Text>
-          </Paper>
-        ) : (
-          <Button
-            loading={isLoading}
-            onClick={async (event) => {
-              event.stopPropagation()
-              const storage = new Storage({
-                area: "local"
+    <div>
+      {extMsg ? (
+        <div>
+          <span>{extMsg}</span>
+        </div>
+      ) : (
+        <button
+          onClick={async (event) => {
+            event.stopPropagation()
+            const storage = new Storage({
+              area: "local"
+            })
+
+            const apiKey = await storage.get("apikey")
+            if (!apiKey) {
+              alert("no api key")
+              return
+            }
+
+            const language = await storage.get("lang")
+            if (!language) {
+              alert("no language set")
+              return
+            }
+
+            setIsLoading(true)
+
+            const tweet = getLeafNodeTextContent(tweetHolder)
+
+            const currentDate = new Date()
+
+            const formattedDate = format(currentDate, "yyyy/MM/dd")
+
+            fetch("https://api.openai.com/v1/chat/completions", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${apiKey}`
+              },
+              body: JSON.stringify({
+                model: "gpt-3.5-turbo",
+                messages: [
+                  {
+                    role: "system",
+                    content: `Current date: ${formattedDate}; reply in language: ${language}`
+                  },
+                  {
+                    role: "user",
+                    content: `这是我读到的一条推文:\n ${tweet}`
+                  },
+                  {
+                    role: "user",
+                    content: `解释这条推文要表达的意思。必要时请加入背景信息介绍。`
+                  }
+                ]
               })
-
-              const apiKey = await storage.get("apikey")
-              if (!apiKey) {
-                alert("no api key")
-                return
-              }
-
-              const language = await storage.get("lang")
-              if (!language) {
-                alert("no language set")
-                return
-              }
-
-              setIsLoading(true)
-
-              const tweet = getLeafNodeTextContent(tweetHolder)
-
-              const currentDate = new Date()
-
-              const formattedDate = format(currentDate, "yyyy/MM/dd")
-
-              fetch("https://api.openai.com/v1/chat/completions", {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                  Authorization: `Bearer ${apiKey}`
-                },
-                body: JSON.stringify({
-                  model: "gpt-3.5-turbo",
-                  messages: [
-                    {
-                      role: "system",
-                      content: `Current date: ${formattedDate}; reply in language: ${language}`
-                    },
-                    {
-                      role: "user",
-                      content: `这是我读到的一条推文:\n ${tweet}`
-                    },
-                    {
-                      role: "user",
-                      content: `解释这条推文要表达的意思。必要时请加入背景信息介绍。`
-                    }
-                  ]
-                })
+            })
+              .then((response) => response.json())
+              .then((data) => {
+                console.log(data)
+                setExt(data.choices[0].message.content)
+                setIsLoading(false)
               })
-                .then((response) => response.json())
-                .then((data) => {
-                  console.log(data)
-                  setExt(data.choices[0].message.content)
-                  setIsLoading(false)
-                })
-                .catch((error) => {
-                  console.log(error)
-                  setIsLoading(false)
-                })
-            }}>
-            生成解释
-          </Button>
-        )}
-      </Box>
-    </ThemeProvider>
+              .catch((error) => {
+                console.log(error)
+                setIsLoading(false)
+              })
+          }}>
+          生成解释
+        </button>
+      )}
+    </div>
   )
 }
 
